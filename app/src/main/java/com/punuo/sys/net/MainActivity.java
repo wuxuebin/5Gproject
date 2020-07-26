bcd9690490ffade2c3294515713725ad28aef287
wu <757169897@qq.com>
2020-07-26 20:33:19 +0800

    5Gproject

--- app/src/main/java/com/punuo/sys/net/MainActivity.java
+++ app/src/main/java/com/punuo/sys/net/MainActivity.java
@@ -1,11 +1,16 @@
 package com.punuo.sys.net;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Message;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.telephony.CellIdentityNr;
 import android.telephony.CellInfo;
 import android.telephony.CellInfoNr;
@@ -13,8 +18,14 @@ import android.telephony.CellSignalStrengthNr;
 import android.telephony.ServiceState;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.punuo.sys.net.push.ProcessTasks;
+import com.punuo.sys.sdk.PnApplication;
 import com.punuo.sys.sdk.activity.BaseActivity;
 import com.punuo.sys.sdk.util.ToastUtils;
 
@@ -31,8 +42,31 @@ public class MainActivity extends BaseActivity {
     private List<CellInfo> cellInfoList;
     private TextView displayIdentity;
     private TextView displaySignal;
+    private TextView netType;
     private static final int SIGN_GET = 0x0001;
 
+    private Button mapbutton;
+
+
+    private SDKReceiver mReceiver;
+
+    /**
+     * 构造广播监听类，监听 SDK key 验证以及网络异常广播
+     */
+    public class SDKReceiver extends BroadcastReceiver {
+
+        public void onReceive(Context context, Intent intent) {
+            String s = intent.getAction();
+
+            if (s.equals(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR)) {
+                Toast.makeText(MainActivity.this,"apikey验证失败，地图功能无法正常使用",Toast.LENGTH_SHORT).show();
+            } else if (s.equals(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_OK)) {
+                Toast.makeText(MainActivity.this,"apikey验证成功",Toast.LENGTH_SHORT).show();
+            } else if (s.equals(SDKInitializer.SDK_BROADCAST_ACTION_STRING_NETWORK_ERROR)) {
+                Toast.makeText(MainActivity.this,"网络错误",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,13 +74,40 @@ public class MainActivity extends BaseActivity {
         telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         displaySignal = findViewById(R.id.display_sign);
         displayIdentity = findViewById(R.id.display_identity);
+        netType = findViewById(R.id.netType);
+
+        mapbutton=findViewById(R.id.mapbutton);
+        IntentFilter iFilter = new IntentFilter();
+        iFilter.addAction(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_OK);
+        iFilter.addAction(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR);
+        iFilter.addAction(SDKInitializer.SDK_BROADCAST_ACTION_STRING_NETWORK_ERROR);
+        mReceiver = new SDKReceiver();
+        registerReceiver(mReceiver, iFilter);
+
         mBaseHandler.sendEmptyMessage(SIGN_GET);
         MainActivityPermissionsDispatcher.is5GConnectedWithCheck(this);
+        mapbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,DynamicDemo.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @NeedsPermission(Manifest.permission.READ_PHONE_STATE)
     void is5GConnected() {
         if (Build.VERSION.SDK_INT >= 29) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
             ServiceState serviceState = telephonyManager.getServiceState();
             try {
                 Method hwOwnMethod = ServiceState.class.getMethod("getHwNetworkType");
@@ -54,8 +115,10 @@ public class MainActivity extends BaseActivity {
                 int result = (int) hwOwnMethod.invoke(serviceState);
                 Log.i("han.chen", "值为：" + result);
                 if (result == 20) {
+                    netType.setText("5g网络");
                     Log.i("han.chen", "5g网络");
                 } else {
+                    netType.setText("非5g网络");
                     Log.i("han.chen", "非5g网络");
                 }
             } catch (Exception e) {
@@ -66,6 +129,16 @@ public class MainActivity extends BaseActivity {
 
     @NeedsPermission({Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION})
     void getAllCellInfo() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         cellInfoList = telephonyManager.getAllCellInfo();
         for (int i = 0; i < cellInfoList.size(); i++) {
             CellInfo cellInfo = cellInfoList.get(i);
@@ -121,7 +194,7 @@ public class MainActivity extends BaseActivity {
                         .append("ssRsrq: ")
                         .append(cellSignalStrengthNr.getSsRsrq())
                         .append("\n")
-                        .append("ssRinr: ")
+                        .append("ssSinr: ")
                         .append(cellSignalStrengthNr.getSsSinr())
                         .append("\n");
                 displaySignal.setText(signal.toString());
